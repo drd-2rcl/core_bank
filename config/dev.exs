594afa59af1f3c8625f245c92f2d1cd53f6e345b
drd-2rcl <drd.rclrd@gmail.com>
2021-07-22 16:15:58 -0300

    Changed hostname

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :core_bank_api, CoreBankApi.Repo,
   username: "postgres",
   password: "postgres",
   database: "core_bank_api_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 

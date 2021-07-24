17564244c5305b728ebc3f3f9c7e22b3da26702f
drd-2rcl <drd.rclrd@gmail.com>
2021-07-24 19:18:28 -0300

    Added function to insert only password_hash in db

--- lib/core_bank_api/user.ex
+++ lib/core_bank_api/user.ex
@@ -17,6 +17,10 @@ defmodule CoreBankApi.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
   @required_params [:name, :age, :email, :password]
 
   schema "users" do
@@ -37,5 +41,12 @@ defmodule CoreBankApi.User do
     |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end

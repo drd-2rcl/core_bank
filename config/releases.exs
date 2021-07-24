39ffb834fc649de9f34ab5396864f8f9c66234bb
drd-2rcl <drd.rclrd@gmail.com>
2021-07-24 12:38:47 -0300

    set elixir, erlang, and node version

---
+++ config/releases.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :core_bank_api, CoreBankApiWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]

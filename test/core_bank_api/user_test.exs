b8a67cbe493ba95f71c2d24c86c607a57de6951f
drd-2rcl <drd.rclrd@gmail.com>
2021-07-24 21:21:48 -0300

    Created factory to user

--- test/core_bank_api/user_test.exs
+++ test/core_bank_api/user_test.exs
@@ -1,21 +1,32 @@
 defmodule CoreBankApi.UserTest do
   use CoreBankApi.DataCase, async: true
 
-  alias CoreBankApi.User
+  import CoreBankApi.Factory
+
+  alias CoreBankApi.{Repo, User}
   alias Ecto.Changeset
 
   describe "changeset/2" do
     test "when all params are valid, returns a valid changeset" do
-      params = %{
-        name: "Teste",
-        age: 23,
-        email: "teste@teste.com",
-        password: "123456"
-      }
+      params = build(:user_params)
 
       response = User.changeset(params)
 
-      assert %Changeset{changes: %{name: "Teste"}, valid?: true} = response
+      assert %Changeset{changes: %{name: "Barry Allen"}, valid?: true} = response
+    end
+
+    test "when there are some error, returns an invalid changeset with info error" do
+      params = build(:user_params, %{age: 13, email: "testeteste.com", password: "123"})
+
+      response = User.changeset(params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["should be at least 6 character(s)"],
+        email: ["has invalid format"]
+      }
+
+      assert errors_on(response) == expected_response
     end
   end
 end

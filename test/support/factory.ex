96c50d775dcf02c50677a19bb7f8b591b97a1405
drd-2rcl <drd.rclrd@gmail.com>
2021-07-24 21:31:13 -0300

    Added doc to factory module

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,4 +1,18 @@
 defmodule CoreBankApi.Factory do
+  @moduledoc """
+  The factory module was created to help setup the tests
+
+  To use it, define a function with the name of the module that will be tested and with the necessary parameters, for example:
+
+  def user_params_factory do
+    %{
+      name: "Barry Allen",
+      age: 23,
+      email: "barry_allen@teamflash.com",
+      password: "123456"
+    }
+  end
+  """
   use ExMachina
 
   def user_params_factory do

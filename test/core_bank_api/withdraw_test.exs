c28c8546613aba870f9113b9c67dfc1ac2aef3a8
drd-2rcl <drd.rclrd@gmail.com>
2021-07-31 15:35:30 -0300

    Created test to withdraw schema

---
+++ test/core_bank_api/withdraw_test.exs
@@ -0,0 +1,38 @@
+defmodule CoreBankApi.WithdrawTest do
+  use CoreBankApi.DataCase, async: true
+
+  import CoreBankApi.Factory
+
+  alias CoreBankApi.Withdraw
+  alias Ecto.Changeset
+
+  describe "changeset/2" do
+    test "when all params are valid, returns a valid changeset" do
+      params = build(:withdraw_params)
+
+      response = Withdraw.changeset(params)
+
+      assert %Changeset{
+               changes: %{
+                 account_id: "41ac7822-5f62-4d49-b124-fe8db9a85dff",
+                 amount: _
+               },
+               valid?: true
+             } = response
+    end
+
+    test "when there are some error, returns an invalid changeset with info error" do
+      params =
+        build(:withdraw_params, %{"account_id" => "", "amount" => ""})
+
+      response = Withdraw.changeset(params)
+
+      expected_response = %{
+        account_id: ["can't be blank"],
+        amount: ["can't be blank"],
+      }
+
+      assert errors_on(response) == expected_response
+    end
+  end
+end

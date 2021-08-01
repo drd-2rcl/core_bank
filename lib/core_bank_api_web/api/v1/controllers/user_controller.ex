b5f317101817f644a2476732dc82faa026192eef
drd-2rcl <drd.rclrd@gmail.com>
2021-08-01 14:34:38 -0300

    Added function sign_in

--- lib/core_bank_api_web/api/v1/controllers/user_controller.ex
+++ lib/core_bank_api_web/api/v1/controllers/user_controller.ex
@@ -3,14 +3,24 @@ defmodule CoreBankApiWeb.Api.V1.UserController do
 
   alias CoreBankApi.User
   alias CoreBankApiWeb.Api.V1.FallbackController
+  alias CoreBankApiWeb.Auth.Guardian
 
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- CoreBankApi.create_user(params) do
+    with {:ok, %User{} = user} <- CoreBankApi.create_user(params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
       conn
       |> put_status(:created)
-      |> render("create.json", user: user)
+      |> render("create.json", token: token, user: user)
+    end
+  end
+
+  def sign_in(conn, params) do
+    with {:ok, token} <- Guardian.authenticate(params) do
+      conn
+      |> put_status(:ok)
+      |> render("sign_in.json", token: token)
     end
   end
 end

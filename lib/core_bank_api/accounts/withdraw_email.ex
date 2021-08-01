db032cea6f53bbd92c19c6391b83471fe6c7031a
drd-2rcl <drd.rclrd@gmail.com>
2021-08-01 11:16:09 -0300

    Created module responsible to send email when a withdrawal is made

---
+++ lib/core_bank_api/accounts/withdraw_email.ex
@@ -0,0 +1,38 @@
+defmodule CoreBankApi.Accounts.WithdrawEmail do
+  @moduledoc """
+  This module is responsible for send email when a withdrawal is made
+
+  This can be used as:
+
+      alias CoreBankApi.Accounts.WithdrawEmail
+      WithdrawEmail.call(%Account{})
+  """
+  alias CoreBankApi.Accounts.Email, as: AccountEmail
+  alias CoreBankApi.{Mailer, Repo, User}
+  alias Bamboo.Email
+
+  def call(account) do
+    account.user_id
+    |> get_user_email()
+    |> send_email()
+  end
+
+  defp get_user_email(user_id) do
+    case Repo.get(User, user_id) do
+      nil -> {:error, "User not found!"}
+      user -> {:ok, user}
+    end
+  end
+
+  defp send_email({:ok, user}) do
+    user.email
+    |> AccountEmail.create()
+    |> Mailer.deliver_now!()
+    |> handle_send()
+
+    # |> IO.inspect()
+  end
+
+  defp handle_send({:error, _error}), do: {:error, "Falha ao enviar o email"}
+  defp handle_send(%Email{}), do: {:ok, "Email enviado com sucesso"}
+end

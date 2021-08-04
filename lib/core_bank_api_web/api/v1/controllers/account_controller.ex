a8df9e93f3acf43318df2ed31cf0b1b6ba701a3f
drd-2rcl <drd.rclrd@gmail.com>
2021-08-04 16:08:42 -0300

    Added function to export report

--- lib/core_bank_api_web/api/v1/controllers/account_controller.ex
+++ lib/core_bank_api_web/api/v1/controllers/account_controller.ex
@@ -3,6 +3,7 @@ defmodule CoreBankApiWeb.Api.V1.AccountController do
 
   alias CoreBankApi.Account
   alias CoreBankApiWeb.Api.V1.FallbackController
+  alias CoreBankApi.FinancialTransaction
 
   action_fallback FallbackController
 
@@ -12,5 +13,19 @@ defmodule CoreBankApiWeb.Api.V1.AccountController do
       |> put_status(:ok)
       |> render("update.json", account: account)
     end
+  end
+
+  def export(conn, %{"id" => id}) do
+    conn
+    |> put_resp_content_type("text/csv")
+    |> put_resp_header("content-disposition", "attachment; filename=\"report.csv\"")
+    |> send_resp(200, csv_content(id))
+  end
+
+  defp csv_content(id) do
+    FinancialTransaction.all_transactions(id)
+    |> CSV.encode
+    |> Enum.to_list
+    |> to_string
   end
 end
